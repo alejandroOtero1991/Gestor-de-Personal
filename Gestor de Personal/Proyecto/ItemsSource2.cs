8fdfc1962789e89a9d812230beed54a94156358d
alejandroOtero1991 <41967353+alejandroOtero1991@users.noreply.github.com>
2019-04-27 19:54:03 +0200

    Bindings

--- Gestor de Personal/Proyecto/ItemsSource2.cs
+++ Gestor de Personal/Proyecto/ItemsSource2.cs
@@ -1,18 +1,19 @@
 ﻿using Proyecto;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ListaEmpleados
 {
-    class ItemsSource
+    class ItemsSource2
     {
         
         // Bind ArrayList with the ListBox    
-        private List<Empleados> LoadListBoxData()
+        private ObservableCollection<Empleados> LoadListBoxData2()
         {
             Add ventanaAñadir = new Add();
-            List<Empleados> itemsList = new List<Empleados>();
-            itemsList.Add(ventanaAñadir.devolverEmpleado());
-            return itemsList;
+            ObservableCollection<Empleados> itemsList2 = new ObservableCollection<Empleados>();
+            itemsList2.Add(ventanaAñadir.devolverEmpleado());
+            return itemsList2;
         }
     }
 }
